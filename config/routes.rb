1a6387989858ff7c968025454cb3a943e0bd2d34
Francis <roccofrancis@gmail.com>
2016-08-23 17:03:43 -0400

    complete lab: kitchen nested forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 FlatironKitchen::Application.routes.draw do
+  resources :recipes, only: [:new, :create, :edit, :update, :show, :index]
+  resources :ingredients, only: [:new, :create, :edit, :update, :show, :index]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +42,7 @@ FlatironKitchen::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
